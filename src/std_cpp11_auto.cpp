2e0a1f63a580e0c07be4e239637c13cd01c238a6
Nezu Mariko <nezuko2038@gmail.com>
2019-04-17 21:07:42 +0900

    20190417

---
+++ src/std_cpp11_auto.cpp
@@ -0,0 +1,15 @@
+#include <cstdio>
+#include <cstdlib>
+
+/**
+ * @brief  autoptr
+ * @since 2018-02-03
+ * 
+ * @return int 
+ */
+int main(void)
+{
+    auto i = 123;
+    printf("%d¥n", i);
+    return 0;
+}
---
+++ src/std_cpp11_nullptr.cpp
@@ -0,0 +1,15 @@
+#include <cstdio>
+#include <cstdlib>
+
+/**
+ * @brief  nullptr
+ * 
+ * @since 2018-02-03
+ * @return int 
+ */
+int main(void)
+{
+    int *i = nullptr;
+    printf("%d", *i);
+    return 0;
+}
